3ee16ae8172e7696773bd165accc146489f2db24
Mathis-Bermond <9985@holbertonstudents.com>
2025-01-17 14:57:08 +0100

    Refactor 1-last_digit.py to simplify last digit calculation and improve output formatting

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-last_d = number % 10 if number >= 0 else (abs(number) % 10)
-str1 = "Last digit of"
-str2 = "is"
-str3 = "and is greater than 5"
-str4 = "and is 0"
-str5 = "and is less than 6 and not 0"
-if last_d > 5:
-    print(f"{str1} {number} {str2} {last_d} {str3}")
-elif last_d == 0:
-    print(f"{str1} {number} {str2} {last_d} {str4}")
-elif last_d < 6 and last_d != 0:
-    print(f"{str1} {number} {str2} {last_d} {str5}")
+nif number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+else:
+    print("Last digit of", number, "is", last_digit,
+          "and is less than 6 and not 0")
