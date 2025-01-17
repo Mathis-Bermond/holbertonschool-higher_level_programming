6e9a23ca4846a69b0de33843e8b5a12e2a9dcce7
Mathis-Bermond <9985@holbertonstudents.com>
2025-01-17 14:47:42 +0100

    Refactor 1-last_digit.py to improve string formatting in output messages

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 last_d = number % 10 if number >= 0 else (abs(number) % 10)
+str1 = "Last digit of"
+str2 = "is"
+str3 = "and is greater than 5"
+str4 = "and is 0"
+str5 = "and is less than 6 and not 0"
 if last_d > 5:
-    print(f"Last digit of {number} is {last_d} and is greater than 5")
+    print(f"{str1} {number} {str2} {last_d} {str3}")
 elif last_d == 0:
-    print(f"Last digit of {number} is {last_d} and is 0")
+    print(f"{str1} {number} {str2} {last_d} {str4}")
 elif last_d < 6 and last_d != 0:
-    print(f"Last digit of {number} is {last_d} and is less than 6 and not 0")
+    print(f"{str1} {number} {str2} {last_d} {str5}")
